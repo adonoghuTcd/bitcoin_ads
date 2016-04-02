60d7c85b6ee2cd5c0224d45b2edd43dd5deffa2d
Andrew Bruce <Andrew@Andrews-MacBook-Pro.local>
2016-04-02 18:42:38 +0100

    First commit

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello world' });
-  $(document).ready(function(e) {
-        $('#popup').animate({"top":"50%","marginTop":"-200px"},500);
-    });
 });
 
 module.exports = router;
-
-
-
